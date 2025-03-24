a0aeed70b7fb635ab0829ec2ccf91a9d677b1c04
Artur Kim <kimarturdev@gmail.com>
2025-03-24 22:30:54 +0300

    YOUTUBE: Initial Commit

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from "react";
+import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return <HomeLayout>{children}</HomeLayout>;
+}
